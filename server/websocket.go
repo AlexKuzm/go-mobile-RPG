2a60bf125a4180362a8b5c1d9b13f2a60b154351
mishuk <mishuk.sk@live.ru>
2019-06-18 15:44:14 +0300

    added basic ws server package

---
+++ server/websocket.go
@@ -0,0 +1,80 @@
+package websocket
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+var (
+	ErrUserNotRegistered = errors.New("User is not registered to establish websocket connection")
+)
+
+// WsServer defines http server, that upgrades connection to websocket and handles it
+type WsServer struct {
+	Upgrader websocket.Upgrader
+	server   http.Server
+	serveMux *http.ServeMux
+	users    map[string]time.Time
+}
+
+// WebsocketHandler represents simple wrapper for function, handling websocket connection
+type WebsocketHandler func(*websocket.Conn)
+
+// ListenAndServe Starts server listening on provided addr with routes specified by AddWebsocketHandler
+func (s *WsServer) ListenAndServe(addr string) {
+	s.server = http.Server{
+		Addr:         addr,
+		Handler:      s.serveMux,
+		ReadTimeout:  15 * time.Second,
+		WriteTimeout: 15 * time.Second,
+	}
+	s.server.ListenAndServe()
+}
+
+// RegisterUser adds user token to server-known users for future connection establishment
+func (s *WsServer) RegisterUser(token string, expiresOn time.Time) error {
+	if _, ok := s.users[token]; ok == true {
+		return fmt.Errorf("User with token %s already exists", token)
+	}
+	s.users[token] = expiresOn
+	return nil
+}
+
+// AddWebsocketHandler adds new WebsocketHandler to server. Must be added BEFORE server starts serving
+func (s *WsServer) AddWebsocketHandler(path string, handleFunc WebsocketHandler) {
+	if s.serveMux == nil {
+		s.serveMux = http.NewServeMux()
+	}
+	s.serveMux.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+		conn, err := s.Upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			log.Printf("Unsuccessful connection upgrade: %v\n", err)
+			return
+		}
+		var token struct {
+			token string `json: "token"`
+		}
+		err = conn.ReadJSON(&token)
+		if err != nil {
+			log.Printf("Unable to read JSON FIRST message from ws connection: %v\n", err)
+			return
+		}
+		if err := s.checkUser(token.token); err != nil {
+			log.Println(err)
+			return
+		}
+		handleFunc(conn)
+	})
+}
+
+func (s *WsServer) checkUser(token string) error {
+	if t, ok := s.users[token]; t.Before(time.Now()) || !ok {
+		return ErrUserNotRegistered
+	}
+	return nil
+}
