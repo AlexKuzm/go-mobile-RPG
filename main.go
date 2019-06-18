f697f9f0e3f3db65669a922e0b176976442651b1
mishuk <mishuk.sk@live.ru>
2019-06-18 19:05:47 +0300

    style fix

--- main.go
+++ main.go
@@ -2,15 +2,13 @@ package main
 
 import (
 	"log"
-	"sync"
 
 	"github.com/mishuk-sk/go-pathfinder/server"
 )
 
 func main() {
 	server := server.NewServer()
-	var wg *sync.WaitGroup
-	wg = server.ServeWithWebsocket()
+	wg := server.ServeWithWebsocket()
 
 	server.WaitShutdown()
 
